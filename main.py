9af342e1b34c00987a6b829a3e22e8a701eb4245
Jaemin Seol <blackseol@gachon.ac.kr>
2022-12-08 18:14:35 +0900

    add webcam view window

--- main.py
+++ main.py
@@ -8,6 +8,29 @@ import os
 #window
 win = tkinter.Tk()
 win.title("Face-recognition")
-win.geometry("1920x1080")
-win.resizable(True, True)
+win.geometry("1280x800")
+win.resizable(False, False)
+videoFrame = tkinter.Frame(win, bg = "white", width=1280, height=720)
+videoFrame.grid(row=1, column=0)
+
+videolb = tkinter.Label(win)
+videolb.grid(row=0, column=0)
+video = cv2.VideoCapture(0)
+
+video.set(4, 720)
+video.set(3, 1280)
+#video
+def video_show():
+
+    status, frame = video.read()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    img = Image.fromarray(frame)
+    imgtk = ImageTk.PhotoImage(image=img)
+
+    videolb.imgtk = imgtk
+    videolb.configure(image=imgtk)
+    videolb.after(10, video_show)
+
+video_show()
 win.mainloop()
