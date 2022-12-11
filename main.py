8118468dd2ed08f31982eee0c372ea698080bb79
Jaemin Seol <blackseol@gachon.ac.kr>
2022-12-11 18:59:09 +0900

    main feature completed. updating GUI...

--- main.py
+++ main.py
@@ -5,22 +5,27 @@ print("Loading... Please wait...")
 import os
 import sys
 import argparse
+import json
+import base64
 import warnings
+from threading import Timer
 import tkinter as tk
+from tkinter import messagebox
 import numpy as np
 from PIL import ImageTk, Image
 import cv2 as cv
 from yunet import YuNet
 from sface import SFace
+np.set_printoptions(threshold = sys.maxsize)
 #argparse
 parser = argparse.ArgumentParser(description="Face detection and recognition using open CV.\
 \n2022. Gachon Univ. OSS team 11.\n202234900 Seol Jaemin\n2022*****NAMEHERE\n2022*****NAMEHERE\n2022*****NAMEHERE")
 parser.add_argument('resolution', help="vertical resolution of window in interger", action="store")
-parser.add_argument('-c', '--camera', help="camera number.", dest="camera_number" ,action="store", default=0)
-parser.add_argument('-l', '--lowSpec', help="Use this option if you are using low spec PC", action="store_true", default=False)
-parser.add_argument('-w', '--showWarning', help="enables warning message", action="store_true", default=False)
+parser.add_argument('--camera', '-c', help="camera number.", dest="camera_number" ,action="store", default=0)
+parser.add_argument('--lowSpec', '-l', help="Use this option if you are using low spec PC", action="store_true", default=False)
+parser.add_argument('--showWarning', '-w', help="enables warning message", action="store_true", default=False)
 parser.add_argument('--cuda', help="use Nvidia gpu accelleratioin", action="store_true", default=False)
-parser.add_argument('-f', '--fullScreen', help="display fullScreen", action="store_true", default=False)
+parser.add_argument('--fullScreen', '-f', help="display fullScreen", action="store_true", default=False)
 
 args = parser.parse_args()
 #setup variables
@@ -110,13 +115,83 @@ def videoGUI():
         sys.exit(1)
     videolb.after(dtime, videoGUI)
 
+count = 4
+def faceRegistrationDelay():
+    global count
+    count = count - 1
+    if count <= 0:
+        count = 4
+        faceRegistration()
+        return
+    topText.config(text="Face Registration starts in {}...".format(count))
+    delay = Timer(1, faceRegistrationDelay)
+    delay.start()
+
 def faceRegistration():
-    #status, frame = video.read()
-    print("Working")
+    topText.config(text="Please select task to do")
+    try:
+        status, frame = video.read()
+        face = detector.infer(frame)
+        faceCount = 0
+        for f in (face if face is not None else []):
+            faceCount = faceCount + 1
+        if faceCount == 1:
+            #encode
+            status, frame = cv.imencode('.jpg', frame)
+            frame = np.array2string(frame)
+            frame = frame[1:-1]
+            face = np.array2string(face)
+            face = face[2:-2]
+            #save it on file
+            jsonObj = {"img":frame, "detect":face}
+            jsonStr = json.dumps(jsonObj)
+            jsonStr = jsonStr.replace("\n", " ")
+            jsonStr = jsonStr + "\n"
+            with open("data.face", "a") as file:
+                file.write(jsonStr)
+
+        elif faceCount > 1:
+            messagebox.showwarning(title="Face Registration error",
+                                    message="Two or more face detected.\nPlease try again.")
+        else:
+            messagebox.showwarning(title="Face Registration error",
+                                    message="Unable to detect face.\nPlease try again.")
+    except:
+        #print("Face Registration error.\nPleas try again.")
+        messagebox.showwarning(title="Face Registration error", message="Please try again.\nIf error continues, reboot the computer")
+    else:
+        messagebox.showinfo(title="Face Registration success", message="Face Registrated.")
 
 def faceRecognition():
-    print("facercog")
+    try:
+        with open("data.face", "r") as file:
+            jsonStr = file.readline()
+            while jsonStr:
+                jsonStr = jsonStr.strip()
+                jsonObj = json.loads(jsonStr)
 
+                frame = jsonObj['img']
+                face = jsonObj['detect']
+
+                frame = frame.split()
+                frame = np.array(frame)
+                frame = frame.astype(np.uint8)
+                frame = cv.imdecode(frame, cv.IMREAD_COLOR)
+
+                face = face.split()
+                face = [face]
+                face = np.array(face)
+                face = face.astype(np.float32)
+
+                #for testing
+                img2 = cv.imread("test.jpg")
+                face2 = detector.infer(img2)
+                result = recognizer.match(frame, face[0][:-1], img2, face2[0][:-1])
+        print(result)
+    except:
+        messagebox.showwarning(title="Face Recognition error", message="Please try it again.\nIf error continues, reboot the computer")
+    else:
+        pass
 def openLog():
     print("openlog")
 
@@ -129,7 +204,7 @@ topText.place(x=0,y=0)
 #face registration button
 btnWidth = int(hres / 3) #3 buttons
 
-faceRegBtn = tk.Button(win, width=btnWidth, height=bres, command=faceRegistration, text="Registration",
+faceRegBtn = tk.Button(win, width=btnWidth, height=bres, command=faceRegistrationDelay, text="Registration",
                 image=pixelImg, compound="c")
 faceRegBtn.place(x=0,y=vres-bres)
 
